c381a67a2aad31ae5288e377df2dd78577bff5fe
StrixIsTheBest <130427869+StrixIsTheBest@users.noreply.github.com>
2024-12-26 12:08:25 +0400

    Update main.py

--- main.py
+++ main.py
@@ -160,6 +160,7 @@ tease_phrases = [
 # Event: Bot is ready
 @bot.event
 async def on_ready():
+    await bot.tree.sync()
     print(f"{bot.user.name} is now online and ready to assist! 🌸 UwU")
     activity = discord.Game(name="Helping you, senpai~ 💖")
     await bot.change_presence(status=discord.Status.online, activity=activity)
